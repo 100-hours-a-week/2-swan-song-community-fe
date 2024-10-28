942d9c79108fcfde71368f35a1b9d34e1f217f73
Unagi-zoso <Unagi.zoso@gmail.com>
2024-10-28 18:11:40 +0900

    Fix: 누락된 부분 추가

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/views', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
 });
