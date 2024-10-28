4ddf1833fcbc08eb6c01c6f7d9fdb1dfd3f69958
Unagi-zoso <Unagi.zoso@gmail.com>
2024-10-28 18:01:57 +0900

    Feat: index layout

---
+++ public/javascripts/GlobalNavbarFunction.js
@@ -0,0 +1,11 @@
+const profileMenuToggle = () => {
+    const profileMenu = document.getElementsByClassName("profile-menu")[0];  
+    if (profileMenu.style.display === "none" || profileMenu.style.display === "") {
+        profileMenu.style.display = "block";
+    } else {
+        profileMenu.style.display = "none";
+    }
+}
+
+const actionListToggle = document.getElementsByClassName("action-list-toggle")[0];
+actionListToggle.addEventListener("click", profileMenuToggle);
