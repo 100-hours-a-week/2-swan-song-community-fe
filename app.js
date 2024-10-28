d91014abf6110720b282dfeda6c9058147d4e92a
Unagi-zoso <Unagi.zoso@gmail.com>
2024-10-28 17:02:10 +0900

    Feat: 초기 세팅

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
+});
