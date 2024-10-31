0735f2850c8e270ac61ce89f7d1b6edd27c09207
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-01 07:17:43 +0900

    Feat viewRouter 분리

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import viewRouter from './routes/viewRouter.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'login.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'register.html'));
-});
-
-app.get('/post-modify', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'post-modify.html'));
-});
-
-app.get('/post-upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'post-upload.html'));
-});
-
-app.get('/post-detail/:id', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/views', 'post-detail.html'));
-});
+app.use(viewRouter);
 
 app.listen(port, () => {
     console.log(`서버가 ${port}번 포트에서 실행 중입니다.`);
