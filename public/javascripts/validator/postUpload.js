6f13dfa99c4c96288e57db0729d52059c9f21d1e
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-05 02:39:37 +0900

    Feat post-upload HelperText 추가 및 스타일 개선

---
+++ public/javascripts/validator/postUpload.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const titleInput = document.querySelector('input[name="title"]');
+    const postContentTextarea = document.querySelector(
+        'textarea[name="postContent"]',
+    );
+    const submitButton = document.querySelector('.post-upload-btn');
+    const helperText = document.querySelector('.helper-text');
+
+    function validateTitle() {
+        if (titleInput.value.length > 26) {
+            titleInput.value = titleInput.value.slice(0, 26);
+        }
+    }
+
+    function updateButtonState() {
+        const isTitleFilled = titleInput.value.trim() !== '';
+        const isContentFilled = postContentTextarea.value.trim() !== '';
+
+        if (isTitleFilled && isContentFilled) {
+            submitButton.style.backgroundColor = '#7f6aee';
+            submitButton.disabled = false;
+            helperText.style.display = 'none'; // 모두 입력 시 헬퍼 텍스트 숨김
+        } else {
+            submitButton.style.backgroundColor = ''; // 버튼 색상 초기화
+            submitButton.disabled = true; // 버튼 비활성화
+            helperText.style.display = 'block'; // 헬퍼 텍스트 표시
+        }
+    }
+
+    titleInput.addEventListener('input', () => {
+        validateTitle();
+        updateButtonState();
+    });
+
+    postContentTextarea.addEventListener('input', updateButtonState);
+
+    updateButtonState(); // 초기 상태 업데이트
+});
