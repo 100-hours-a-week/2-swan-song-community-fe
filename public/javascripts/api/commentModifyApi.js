21a51a37b288b5e049d38f66e34135179c7acb69
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-19 15:16:04 +0900

    Feat 댓글 수정 프론트 1차 완료

---
+++ public/javascripts/api/commentModifyApi.js
@@ -0,0 +1,29 @@
+const updatePostComment = async () => {
+    const commentInputTextarea = document.getElementById('commentInputFormTextarea');
+    const commentId = commentInputTextarea.dataset.commentId;
+
+    const content = commentInputTextarea.value;
+    const response = await fetch('/api/v1/posts/comments', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ commentId, content }),
+    });
+
+    if (response.ok) {
+        const responseJson = await response.json();
+        if (responseJson.code === 2000) {
+            const commentList = document.querySelector('.comment-list');
+            const commentEl = commentList.find((el) => el.dataset.commentId === commentId);
+            commentEl.querySelector('.comment-content p').textContent = content;
+
+            commentInputTextarea.value = '';
+
+            const commentInputFormButton = document.getElementById('commentInputFormButton');
+            commentInputFormButton.textContent = '댓글 작성';
+            commentInputFormButton.removeEventListener('click', updatePostComment);
+            commentInputFormButton.addEventListener('click', uploadPostComment);
+        }
+    }
+};
