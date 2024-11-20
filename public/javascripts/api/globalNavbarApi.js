8bf3c89c1ae698e4183cb92a068341e8ac82976f
Unagi-zoso <Unagi.zoso@gmail.com>
2024-11-20 11:46:14 +0900

    Feat navbar 에 로그인 사용자 프로필 표현 구현

--- public/javascripts/api/globalNavbarApi.js
+++ public/javascripts/api/globalNavbarApi.js
@@ -1,3 +1,20 @@
+const profileDropDownToggle = document.getElementById('profileDropDownToggle');
+document.addEventListener('DOMContentLoaded', getProfile);
+
+async function getProfile() {
+    const response = await fetch('/api/v1/users/me', {
+        method: 'GET',
+    });
+
+    const data = await response.json();
+
+    if (data.code === 2000) {
+        profileDropDownToggle.src =
+            data.data.profileImageUrl ||
+            '/images/assets/User_Default_Profile.svg';
+    }
+}
+
 const logoutUser = async () => {
     try {
         const response = await fetch('/api/v1/auth/logout', {
